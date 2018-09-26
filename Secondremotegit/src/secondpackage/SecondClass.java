fb72b7b7c810b94576aefbf74faf14ab343fe816
chidiebereokoli <chidiebereokoli@gmail.com>
2018-09-25 19:23:34 -0500

    first time

---
+++ Secondremotegit/src/secondpackage/SecondClass.java
@@ -0,0 +1,10 @@
+package secondpackage;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello comrade");
+	}
+
+}
